6311ce518661aa962e7a5df78fc01fd6d424d0a1
Md. Nurusshafi Evan <76874563+N-Evan@users.noreply.github.com>
2023-07-04 18:39:30 +0600

    Update CreditController.cs

--- Assets/Tools23.CreditsSystem/Scripts/CreditController.cs
+++ Assets/Tools23.CreditsSystem/Scripts/CreditController.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Tools23.CreditSystem.ScriptableObjects;
 using UnityEngine;
 
@@ -10,8 +10,34 @@ namespace Tools23.CreditSystem.Core
 		public GameObject HeaderSection;
 		public GameObject FooterSection;
 
+		public Transform ContentParent;
+
 		[SerializeField] private GameObject _sectionPrefab;
 
 		public List<CreditSectionContentScriptableObject> Sections = new List<CreditSectionContentScriptableObject>();
+
+		public void GenerateSections()
+		{
+			ClearSections();
+			foreach (var creditSection in Sections)
+			{
+				var sectionObj = Instantiate(_sectionPrefab);
+				sectionObj.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = creditSection.SectionRole;
+				var sectionNames = sectionObj.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+				sectionNames.text = "";
+				foreach (var members in creditSection.SectionMembers)
+				{
+					sectionNames.text += members + "\n";
+				}
+			}
+		}
+
+		private void ClearSections()
+		{
+			foreach (Transform child in ContentParent)
+			{
+				Destroy(child.gameObject);
+			}
+		}
 	}
 }
