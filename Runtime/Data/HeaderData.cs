c764c9dfba7911f5833d874aba666689c703af27
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2024-01-29 12:06:10 +0600

    Removed Unncessary Extension

---
+++ Runtime/Data/HeaderData.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace Studio23.SS2.CreditsSystem.Data
+{
+	[Serializable]
+	public class HeaderData 
+	{
+		public Sprite HeaderImage;
+		public string MainHeaderText;
+		public string SubHeaderText;
+		public string OptionalHeaderText;
+	}
+}
